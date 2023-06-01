61642dc47af1163f7d3efe3448b6b237066fa406
Jamshid K <jamshid.k@zeeve.io>
2023-06-01 06:13:12 +0530

    postgres production db

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ from flask_migrate import Migrate
 
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
+import os
+from dotenv import load_dotenv
 
 
 def create_app(db_url=None):
     app = Flask(__name__)
-
+    load_dotenv()
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
@@ -21,7 +23,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     Migrate(app, db)
